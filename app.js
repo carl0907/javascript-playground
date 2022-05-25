492a7a1a460b735dab670c420452a5e078b71f73
unknown <michael.cj143@gmail.com>
2022-05-25 12:41:55 +0800

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-window.onload = () => {
+﻿/*window.onload = () => {
     //console.log('This is your first program executing!');
+    console.log('My Name is CJ');
+}*/
+if (typeof window !== 'undefined') {
+    console.log('You are on the browser')
+    // ✅ Can use window here 1
+} else {
+    console.log('You are on the server')
+    // ⛔️ Don't use window here 2
 }
